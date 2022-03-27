d4f7631cd1fdf1c8cdc66eefe08960074661268f
Jakub Kowalczyk <92227247+Sqzjelele@users.noreply.github.com>
2022-03-27 10:29:54 +0200

    Add files via upload

---
+++ Lab1/src/com/company/Lab1_ex2.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Lab1_ex2 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input data to file: ");
+        String line = scanner.nextLine();
+
+        try (FileOutputStream file = new FileOutputStream("file.txt")) {
+            byte[] mybytes = line.getBytes();
+
+            file.write(mybytes);
+        }
+    }
+}
